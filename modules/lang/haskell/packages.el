570bdb0a18ab5dfcdcba5708db2055ce15e6a108
Edmund Miller <Edmund.A.Miller@protonmail.com>
2018-06-23 12:53:51 -0500

    Fix missing paranthesis
    Missing parathesis after `depends-on!`

--- modules/lang/haskell/packages.el
+++ modules/lang/haskell/packages.el
@@ -4,12 +4,12 @@
 (package! haskell-mode)
 
 (cond
- ((featurep! +lsp) (depends-on! :tools lsp
-                                (package! lsp-haskell)))
+ ((featurep! +lsp) (depends-on! :tools lsp)
+  (package! lsp-haskell)))
  ((featurep! +dante)
        (package! dante)
        (when (featurep! :completion company)
          (package! company-ghc)))
       (t
        (package! intero)
-       (package! hindent)))
+       (package! hindent))
