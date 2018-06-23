eb2f5f20111f724b458a19549b3da886a02428e5
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-06-23 14:22:53 +0200

    added quick and dirty lsp support to haskell module

--- modules/lang/haskell/packages.el
+++ modules/lang/haskell/packages.el
@@ -4,9 +4,13 @@
 (package! haskell-mode)
 
 ;;
-(cond ((featurep! +dante)
-       (package! dante))
+(cond
+ ((featurep! +lsp) (depends-on! :tools lsp
+                                (package! lsp-haskell)))
+ ((featurep! +dante)
+       (package! dante)
+       (when (featurep! :completion company)
+         (package! company-ghc)))
       (t
        (package! intero)
        (package! hindent)))
-
