ecd2d1ab6fc081c2afe8a73c67b3617bdf3b2f23
Mark Redeman <markredeman@gmail.com>
2018-11-17 18:18:42 +0100

    [lang/php] WIP

---
+++ modules/app/telegram/packages.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; -*-
+;;; app/telegram/packages.el
+
+;(use-package telega
+;  :recipe (:fetcher github :repo "zevlg/telega.el")
+;  :commands (telega)
+;  :defer t)
+
+;; https://github.com/sarg/dotfiles/blob/master/emacs/.doom.d/modules/app/telega/packages.el
+;; (package! telega
+;;   :load-path "/home/mark/Programs/telega.el"
+
+         ;; :recipe (:fetcher
+         ;;          github
+         ;;          :repo "zevlg/telega.el"
+         ;;          :files ("telega.el")
+         ;;          )
+         ;; )
--- modules/lang/php/packages.el
+++ modules/lang/php/packages.el
@@ -2,18 +2,20 @@
 ;;; lang/php/packages.el
 
 (package! php-boris)
+(package! psysh)
 (package! php-extras :recipe (:fetcher github :repo "arnested/php-extras"))
 (package! php-mode)
 (package! composer)
-(package! php-refactor-mode)
+;; (package! php-refactor-mode)
 (package! php-cs-fixer)
 (package! phpactor)
 (package! flycheck-phanclient :recipe (:fetcher github :repo "TysonAndre/flycheck-phanclient"))
 (unless (featurep! +lsp)
   (package! company-phpactor))
 
-(when (featurep! :feature syntax-checker)
-  (package! flycheck-phpstan))
+(when (featurep! :tools flycheck)
+  (package! flycheck-phpstan)
+  (package! flycheck-phanclient :recipe (:fetcher github :repo "TysonAndre/flycheck-phanclient")))
 (package! phpunit)
 
 (when (featurep! +hack)
